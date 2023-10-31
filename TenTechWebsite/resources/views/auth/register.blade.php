e92774c39f7f47a105631edb1353dbca90904c88
Ali <123903017+AQ-GB@users.noreply.github.com>
2023-10-31 23:26:00 +0000

    remove name presence from view/db/model

--- TenTechWebsite/resources/views/auth/register.blade.php
+++ TenTechWebsite/resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
+<!-- changed the Register Tab Title -->
+<title>Register</title>
 
 @section('content')
 <div class="container">
@@ -11,13 +13,13 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-
-
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" >
+                                <small id="emailHelp" class="form-text text-muted">We pinky promise to never share your email with anyone</small>
+                                
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -31,7 +33,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -45,7 +47,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
                             </div>
                         </div>
 
