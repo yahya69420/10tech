1d7878765fe16d38056b8888fbe5144f7c2c36fd
dabigbulletz638 <m.by__sana@hotmail.com>
2023-11-12 21:54:06 +0000

    Add route to about page

--- TenTechWebsite/routes/web.php
+++ TenTechWebsite/routes/web.php
@@ -22,6 +22,10 @@ Route::get('/shop', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
