684fc594a214bde17397dcf26f41734ebc041d77
Ali <123903017+AQ-GB@users.noreply.github.com>
2023-12-09 20:38:04 +0000

    code formatting;login/logout/regiser conditional added

--- TenTechWebsite/resources/views/header.blade.php
+++ TenTechWebsite/resources/views/header.blade.php
@@ -1,55 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>10tech</title>
-    <link rel="stylesheet" href="{{ asset('/css/headerstyle.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>10tech</title>
+  <link rel="stylesheet" href="{{ asset('/css/headerstyle.css') }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
 
-<header class="header">
+  <header class="header">
     <section class="p-menu1">
-        <nav id="navbar" class="navigation" role="navigation">
-          <input id="toggle1" type="checkbox" />
-          <label class="hamburger1" for="toggle1">
-            <div class="top"></div>
-            <div class="meat"></div>
-            <div class="bottom"></div>
-          </label>
+      <nav id="navbar" class="navigation" role="navigation">
+        <input id="toggle1" type="checkbox" />
+        <label class="hamburger1" for="toggle1">
+          <div class="top"></div>
+          <div class="meat"></div>
+          <div class="bottom"></div>
+        </label>
 
-          <nav class="menu1">
-            <a class="link1" href="Laptop">Laptops</a>
-            <a class="link1" href="mobile">Smartphones</a>
-            <a class="link1" href="Tablet">Tablets</a>
-            <a class="link1" href="Console">Console</a>
+        <nav class="menu1">
+          <a class="link1" href="Laptop">Laptops</a>
+          <a class="link1" href="mobile">Smartphones</a>
+          <a class="link1" href="Tablet">Tablets</a>
+          <a class="link1" href="Console">Console</a>
 
-          </nav>
         </nav>
+      </nav>
 
-      </section>
-    <div class = "search">
-        <input type="search" placeholder = "Search...">
+    </section>
+    <div class="search">
+      <input type="search" placeholder="Search...">
     </div>
     <div class="logo">
-        <h1>TenTech</h1>
+      <h1>TenTech</h1>
     </div>
     <nav class="navigation">
-        <ul>
-            <li class = "dropdown"><button class="dropbtn"><img src = "account.png" class = "account"></button>
-              <div class="dropdown-content">
-                <a href="login">Login</a>
-                <a href="register">Register</a>
-              </div></li>
-              <li class = "dropdown"><button class="dropbtn"><img src = "shopping-basket.png" class = "account"></button>
-              <div class="dropdown-content">
-                <a href="basket">Basket</a>
-                <a href="checkout">Checkout</a>
-              </div></li>
-        </ul>
+      <ul>
+        <li class="dropdown"><button class="dropbtn"><img src="account.png" class="account"></button>
+          <div class="dropdown-content">
+             <!-- if user authenticated, show POST logout button with CSRF token for security -->
+            @if (Auth::check()) 
+            <form action="{{ route('logout') }}" method="POST">
+              @csrf
+              <button type="submit">Logout</button>
+            </form>
+            @else
+            <!-- if user is not authenticated, show login and register links -->
+            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('register') }}">Register</a>
+            @endif
+          </div>
+        </li>
+        <li class="dropdown"><button class="dropbtn"><img src="shopping-basket.png" class="account"></button>
+          <div class="dropdown-content">
+            <a href="basket">Basket</a>
+            <a href="checkout">Checkout</a>
+          </div>
+        </li>
+      </ul>
     </nav>
-</header>
-
+  </header>
 </body>
 </html>
