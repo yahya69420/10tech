2760f16062882e2190cca11cb5ee4098d04d6a32
dabigbulletz638 <m.by__sana@hotmail.com>
2023-12-09 19:14:22 +0000

    fix login page too

--- TenTechWebsite/resources/views/auth/login.blade.php
+++ TenTechWebsite/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="stylesheet" href="{{ asset('../resources/css/loginPage.css') }}">
+    <link rel="stylesheet" href="{{ asset('loginPage.css') }}">
 
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
