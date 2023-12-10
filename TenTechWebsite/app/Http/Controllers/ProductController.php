0077b3585b9861157e5715e6f3a79198dc5d376c
Ali <123903017+AQ-GB@users.noreply.github.com>
2023-12-10 15:47:05 +0000

    added display functions for all products to each view

--- TenTechWebsite/app/Http/Controllers/ProductController.php
+++ TenTechWebsite/app/Http/Controllers/ProductController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
     public function index()
     {
         $products = Product::all();
-
-        return view('welcome', ['products' => $products]);
+        $categories = Category::all();
+        return view('welcome', ['products' => $products, 'categories' => $categories]);
     }
 
     public function productDetail($productId)
@@ -19,4 +20,48 @@ class ProductController extends Controller
         $product = Product::find($productId);
         return view('productdetail', ['product' => $product]);
     }
+
+    public function showAllConsoles() 
+    {
+        $consoles = Product::join('category_product', 'products.id', '=', 'category_product.product_id')
+            ->where('category_product.category_id', 2)
+            ->get();
+
+        return view('console', ['consoles' => $consoles]);
+    }
+
+    public function showAllMobiles() 
+    {
+        $mobiles = Product::join('category_product', 'products.id', '=', 'category_product.product_id')
+            ->where('category_product.category_id', 1)
+            ->get();
+
+        return view('mobile', ['mobiles' => $mobiles]);
+    }
+
+    public function showAllMonitors() 
+    {
+        $monitors = Product::join('category_product', 'products.id', '=', 'category_product.product_id')
+            ->where('category_product.category_id', 3)
+            ->get();
+
+        return view('monitor', ['monitors' => $monitors]);
+    }
+
+    public function showAllTablets() {
+        $tablets = Product::join('category_product', 'products.id', '=', 'category_product.product_id')
+            ->where('category_product.category_id', 4)
+            ->get();
+
+        return view('tablet', ['tablets' => $tablets]);
+    } 
+
+    public function showAllLaptops() {
+        $laptops = Product::join('category_product', 'products.id', '=', 'category_product.product_id')
+            ->where('category_product.category_id', 5)
+            ->get();
+
+        return view('laptop', ['laptops' => $laptops]);
+    }
+    
 }
