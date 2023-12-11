35fa97e98fefb110371292a7b3926f778519b642
Ali <123903017+AQ-GB@users.noreply.github.com>
2023-12-11 08:51:59 +0000

    created Cart model and subsequent database table

---
+++ TenTechWebsite/database/migrations/2023_12_11_005736_create_cart_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            // create a foreign key that references the id column in the users table
+            $table->foreignId('user_id')->constrained();
+            // create a foreign key that references the id column in the products table
+            $table->foreignId('product_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart');
+    }
+};
