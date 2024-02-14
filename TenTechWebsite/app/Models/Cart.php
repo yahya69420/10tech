0ab0ce593ae8f8d6363437b06d68755b6278dadd
Ali <123903017+AQ-GB@users.noreply.github.com>
2024-02-14 21:40:15 +0000

    expand Cart model fillable array to meet update columns

--- TenTechWebsite/app/Models/Cart.php
+++ TenTechWebsite/app/Models/Cart.php
@@ -10,4 +10,5 @@ class Cart extends Model
     use HasFactory;
     // set the table name to the cart table
     public $table = 'cart';
+    protected $fillable = ['user_id', 'product_id', 'quantity', 'total'];
 }
