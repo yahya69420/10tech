19c56d25bed8d08894062e37d66934d8e1abda79
Ali <123903017+AQ-GB@users.noreply.github.com>
2024-02-14 23:31:52 +0000

    added sweetalerts to communicate with user

--- TenTechWebsite/resources/views/basket.blade.php
+++ TenTechWebsite/resources/views/basket.blade.php
@@ -20,178 +20,182 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- BoxIcon CDN -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Basket</title>
     <style>
         /* remove any default padding and margins to avoid 
 any issues that are impossible to debug later; make this universal*/
 
-/* For clarity, use of tailwind utilities were chosen against here 
+        /* For clarity, use of tailwind utilities were chosen against here 
 so that the css was more easier to read and could be applied 
 universally as is the case here :) */
 
-/* default padding and border arithemetic is like so: 
+        /* default padding and border arithemetic is like so: 
     - width = width + padding + border
     - height = height + padding + border
     border-box removes this for easier arithmetic and modifications when needed */
 
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
 
-html:root {
-    background-color: rgb(238, 238, 238);
-}
+        html:root {
+            background-color: rgb(238, 238, 238);
+        }
 
-body,
-html {
-    font-family: "Roboto", sans-serif;
-    color: black;
-    display: flex;
-    flex-direction: row;
-}
+        body,
+        html {
+            font-family: "Roboto", sans-serif;
+            color: black;
+            display: flex;
+            flex-direction: row;
+        }
 
-body {
-    /* change the background to default to light mode */
-    background-color: rgb(238, 238, 238);
-    display: flex;
-}
+        body {
+            /* change the background to default to light mode */
+            background-color: rgb(238, 238, 238);
+            display: flex;
+        }
 
-#myBagCardId {
-    background-color: rgb(255, 255, 255);
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 20px;
-    width: auto;
-    max-height: 100%; /*Set a fixed height for the container/*
+        #myBagCardId {
+            background-color: rgb(255, 255, 255);
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            margin: 20px;
+            width: auto;
+            max-height: 100%;
+            /*Set a fixed height for the container/*
     
     /*
     Use overflow-auto to add scrollbars to an element in the event that its content 
     overflows the bounds of that element. Unlike overflow-scroll, which always shows 
     scrollbars, this utility will only show them if scrolling is necessary.*/
 
-    overflow-y: auto; /* ennabele vertical scrolling */
-    margin-left: 250px;
-    margin-right: auto;
+            overflow-y: auto;
+            /* ennabele vertical scrolling */
+            margin-left: 250px;
+            margin-right: auto;
 
-}
+        }
 
-.itemList {
-    list-style: none;
-    padding: 10px;
-    border: rgb(22, 21, 21);
-}
+        .itemList {
+            list-style: none;
+            padding: 10px;
+            border: rgb(22, 21, 21);
+        }
 
-.productItem {
-    display: flex;
-    align-items: center;
-    justify-content: start;
-    padding: 10px 0; /* Add padding to separate items */
-    border-bottom: 1px solid #242424; 
-}
+        .productItem {
+            display: flex;
+            align-items: center;
+            justify-content: start;
+            padding: 10px 0;
+            /* Add padding to separate items */
+            border-bottom: 1px solid #242424;
+        }
 
-#blackLine {
-    border: 1px solid rgb(255, 255, 255);
-    background-color: rgb(255, 255, 255);
-    height: 2px;
-}
+        #blackLine {
+            border: 1px solid rgb(255, 255, 255);
+            background-color: rgb(255, 255, 255);
+            height: 2px;
+        }
 
-.productImage {
-    height: auto;
-    width: 80px; 
-    margin-right: 20px; 
-}
+        .productImage {
+            height: auto;
+            width: 80px;
+            margin-right: 20px;
+        }
 
-.productInfo {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    flex-grow: 1;
-}
+        .productInfo {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex-grow: 1;
+        }
 
-.pName,
-.pQuantity,
-.pPrice {
-    margin-bottom: 10px;
-}
+        .pName,
+        .pQuantity,
+        .pPrice {
+            margin-bottom: 10px;
+        }
 
-.quantityDropdown {
-    margin-right: 10px;
-    background-color: white;
-}
+        .quantityDropdown {
+            margin-right: 10px;
+            background-color: white;
+        }
 
-.removeButton {
-    display: flex;
-    justify-content: flex-end; 
-    flex-grow: 0;
-}
+        .removeButton {
+            display: flex;
+            justify-content: flex-end;
+            flex-grow: 0;
+        }
 
-.subTotal {
-    display: flex;
-    align-items: center;
-    justify-content: space-between; 
-    margin-top: 20px;
-}
+        .subTotal {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: 20px;
+        }
 
-/* delivery section styling */
+        /* delivery section styling */
 
-#deliveryCardId {
-    background-color: rgb(238, 238, 238);
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin-bottom: 20px;
-    width: 313px;
-    height: 160px;
-}
+        #deliveryCardId {
+            background-color: rgb(238, 238, 238);
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            margin-bottom: 20px;
+            width: 313px;
+            height: 160px;
+        }
 
-.deliveryForm label {
-    display: block;
-}
+        .deliveryForm label {
+            display: block;
+        }
 
-.deliveryForm input[type="radio"] {
-    background-color: white;
-}
-
-
-#totalCardId {
-    background-color: rgb(255, 255, 255);
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    margin-top: 0px;
-    width: fit-content;
-    height: fit-content;
-}
-
-.rightSide {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 20px;
-    width: fit-content;
-    height: fit-content;
-}
-
-#promoCodeInput {
-    background-color: rgb(255, 255, 255);
-}
-
-.totalInfo {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 20px;
-}
+        .deliveryForm input[type="radio"] {
+            background-color: white;
+        }
 
 
-.checkoutButton {
-    background-color: #049f47; 
-    
-}
-</style>
+        #totalCardId {
+            background-color: rgb(255, 255, 255);
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+            margin-top: 0px;
+            width: fit-content;
+            height: fit-content;
+        }
+
+        .rightSide {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            margin: 20px;
+            width: fit-content;
+            height: fit-content;
+        }
+
+        #promoCodeInput {
+            background-color: rgb(255, 255, 255);
+        }
+
+        .totalInfo {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: 20px;
+        }
+
+
+        .checkoutButton {
+            background-color: #049f47;
+
+        }
+    </style>
 </head>
 
 
@@ -202,11 +206,13 @@ body {
         <!-- Card title -->
         <h1 class="text-3xl font-bold text-center p-5 pb-0 mb-4">My Bag </h1>
         <ul class="itemList">
-        @foreach ($cartItems as $cartItem)
+            @foreach ($cartItems as $cartItem)
             <li class="productItem">
                 <img class="productImage" src="{{ $cartItem->image }}">
                 <div class="productInfo">
-                    <a href="{{ route('productdetail', ['id' => $cartItem->id]) }}" style="color:blue"><div class="pName font-bold">{{ $cartItem->name }}</div></a>
+                    <a href="{{ route('productdetail', ['id' => $cartItem->id]) }}" style="color:blue">
+                        <div class="pName font-bold">{{ $cartItem->name }}</div>
+                    </a>
                     <form action="{{ route('update_cart', ['cart_id' => $cartItem->cart_id]) }}" method="POST">
                         @csrf
                         <input type="hidden" name="cart_id" value="{{ $cartItem->cart_id }}">
@@ -218,7 +224,7 @@ body {
                         <div class="pQuantity font-bold">
                             Quantity:
                             <select class="quantityDropdown productQuantity" data-price="{{ $cartItem->price }}" data-cart-id="{{ $cartItem->cart_id }}" name="product_quantity">
-                                @for ($i = 1; $i <= $cartItem->stock && $i <= 10; $i++) <option value="{{ $i }}" {{ $cartItem->cart_quantity == $i ? 'selected' : '' }}>{{ $i }}</option>
+                                @for ($i = 1; $i <= $cartItem->stock && $i <= $cartItem->cart_quantity; $i++) <option value="{{ $i }}" {{ $cartItem->cart_quantity == $i ? 'selected' : '' }}>{{ $i }}</option>
                                         @endfor
                             </select>
                         </div>
@@ -226,8 +232,7 @@ body {
                             <button class="btn btn-active text-white">Update Cart</button>
                         </div>
                     </form>
-
-                    <div class="pPrice pSubtotal font-bold">£{{ $cartItem->price * $cartItem->cart_quantity }}</div>
+                    <div class="pPrice pSubtotal font-bold">£{{ number_format($cartItem->price * $cartItem->cart_quantity, 2) }}</div>
                 </div>
                 <div class="removeButton">
                     <a href="{{ route('remove_from_basket', ['cart_id' => $cartItem->cart_id]) }}">
@@ -237,8 +242,7 @@ body {
                 @endforeach
             </li>
         </ul>
-
-        @php 
+        @php
         $total = 0;
         @endphp
 
@@ -252,7 +256,7 @@ body {
             <div class="subtotalInfo">
                 <h1 class="font-bold text-xl mt-2">SUBTOTAL</h1>
             </div>
-            <h2 class="font-bold text-l mt-2 ml-2" id="overallSubtotal">£{{ $total }}</h2>
+            <h2 class="font-bold text-l mt-2 ml-2" id="overallSubtotal">£{{ number_format($total, 2) }}</h2>
         </div>
 
 
@@ -273,27 +277,27 @@ body {
             </div>
             <div class="deliveryCard ">
                 <hr id="blackLine">
-    
+
                 <div class="deliveryForm text-xl font-bold">
                     <form action="#" method="POST">
-    
+
                         <label for="deliveryOption1">
                             <input type="radio" id="deliveryOption1" name="deliveryOption" value="option1" checked>
                             Delivery Option 1
                         </label>
-    
+
                         <label for="deliveryOption2">
                             <input type="radio" id="deliveryOption2" name="deliveryOption" value="option2">
                             Delivery Option 2
                         </label>
-    
+
                     </form>
-    
+
                 </div>
-    
-    
+
+
             </div>
-    
+
             <div class="totalInfo">
                 <div class="leftsideInfo">
                     <h2 class="font-bold text-xl mt-2">Estimated shipping costs</h2>
@@ -309,7 +313,7 @@ body {
                     <hr id="blackLine">
                     <h2 class="font-bold text-xl mt-2 ml-2">£0.00</h2>
                     <hr id="blackLine">
-                    <h2 class="font-bold text-xl mt-2 ml-2" id="overallSubtotal2">£{{ $total }}</h2>
+                    <h2 class="font-bold text-xl mt-2 ml-2" id="overallSubtotal2">£{{ number_format($total, 2) }}</h2>
                     <hr id="blackLine">
                 </div>
             </div>
@@ -322,36 +326,25 @@ body {
 
 
 
-<!-- <script>
-    // Function to calculate the total
-    function calculateTotal() {
-        let total = 0;
-
-
-        document.querySelectorAll('.productQuantity').forEach(function(dropdown) {
-            let quantity = Number(dropdown.value);
-            let price = Number(dropdown.getAttribute('data-price'));
-            let subtotal = quantity * price;
-            total += subtotal;
-        });
-
-
-
-
-        document.getElementById('overallSubtotal').textContent = '£' + total.toFixed(2);
-        document.getElementById('overallSubtotal2').textContent = '£' + total.toFixed(2);
-    }
-
-    // Add event listeners to the quantity dropdowns
-    document.querySelectorAll('.productQuantity').forEach(function(dropdown) {
-        dropdown.addEventListener('change', calculateTotal);
+@if(session('success'))
+<script>
+    Toast = Swal.mixin({
+        toast: true,
+        position: "top",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.resumeTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
     });
+    Toast.fire({
+        icon: "success",
+        title: "{{ session('success') }}",
+    });
+</script>
+@endif
 
-
-
-
-    // Calculate the initial total
-    calculateTotal();
-</script> -->
 
 </html>
