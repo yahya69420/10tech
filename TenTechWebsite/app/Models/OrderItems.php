43bff051d3f9b06d217ddeac0a0dea5dbc5610a3
Ali <123903017+AQ-GB@users.noreply.github.com>
2024-02-21 20:55:32 +0000

    modified orderitems model and talbe

--- TenTechWebsite/app/Models/OrderItems.php
+++ TenTechWebsite/app/Models/OrderItems.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItems extends Model
 {
     use HasFactory;
+    protected $fillable = ['quantity', 'created_at', 'updated_at', 'order_id', 'product_id'];
 }
--- TenTechWebsite/database/migrations/2024_02_20_145603_create_order_items_table.php
+++ TenTechWebsite/database/migrations/2024_02_20_145603_create_order_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
