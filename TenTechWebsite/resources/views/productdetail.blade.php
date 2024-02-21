92839e0359cea3f3b3657dbf95d4bfdd486bfef9
Rahul Vasantlal <rahulvasantlal@gmail.com>
2024-02-21 10:00:53 +0000

    readded title tag for indiviudal products

--- TenTechWebsite/resources/views/productdetail.blade.php
+++ TenTechWebsite/resources/views/productdetail.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{ asset('/css/productdetail.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
+    <title> {{$product->name}}</title>
 </head>
 
 <body>
