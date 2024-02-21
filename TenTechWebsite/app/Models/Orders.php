b70e6ef12c9cf609acadc8fa020fa517710053f2
Ali <123903017+AQ-GB@users.noreply.github.com>
2024-02-21 20:54:05 +0000

    modified orders model and talbe

--- TenTechWebsite/app/Models/Orders.php
+++ TenTechWebsite/app/Models/Orders.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Orders extends Model
 {
     use HasFactory;
+    protected $fillable = ['total_before_discount', 'discount_amount', 'total_after_discount', 'status', 'order_date', 'tracking_number', 'user_address_id', 'user_payment_id', 'discount_id', 'user_id', 'created_at', 'updated_at'];
 }
--- TenTechWebsite/database/migrations/2024_02_20_145613_create_orders_table.php
+++ TenTechWebsite/database/migrations/2024_02_20_145613_create_orders_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total_before_discount', 8, 2);
+            $table->decimal('discount_amount', 8, 2)->nullable();
+            $table->decimal('total_after_discount', 8, 2);
+            $table->enum('status', ['pending', 'processing', 'completed', 'cancelled'])->default('pending');
+            $table->timestamp('order_date')->nullable();
+            $table->string('tracking_number')->nullable();
+            $table->foreignId('user_address_id')->references('id')->on('user_addresses')->onDelete('cascade');
+            $table->foreignId('user_payment_id')->references('id')->on('user_payments')->onDelete('cascade');
+            $table->foreignId('discount_id')->nullable()->references('id')->on('discounts')->onDelete('cascade')->nullable();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
 
         });
