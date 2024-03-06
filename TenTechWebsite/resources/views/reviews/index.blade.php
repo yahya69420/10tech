cd4906b859ba205326481651cbb0a32556157920
Rahul Vasantlal <rahulvasantlal@gmail.com>
2024-03-06 18:17:14 +0000

    update the index blade to make sure correctly works with ReviewController

--- TenTechWebsite/resources/views/reviews/index.blade.php
+++ TenTechWebsite/resources/views/reviews/index.blade.php
@@ -1,49 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <title>Write a Review</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    </head>
+    <title>Write a Review</title>
 
-    <body>
+</head>
+
+<body>
     @include('header')
 
-    <div class="container">
-        <div class = "row">
-            <div class = "col-md-12">
-                <div class="card-body">
-                    @if ($verified_purchase->count() > 0)
+    <section class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        @if ($verified_purchase)
                         <h5> You are writing a review for {{ $product->name}} </h5>
-                        <form action= "" method="POST">
-                                <input type= "hidden" name = "product_id" value = "{{ $product->id }}">
-                                <textarea class = "form-control" name = "user_review" rows= "5" placeholder= "Write a review" ></textarea>
-                                <button type = "submit" class = "btn btn-primary"> Submit Review</button>
+
+                        <form action="{{ url('/add-review') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="product_id" value="{{ $product->id }}">
+                            <textarea class="form-control" name="user_review" rows="5" placeholder="Write a review"></textarea>
+                            <button type="submit" class="btn btn-primary mt-3"> Submit Review</button>
                         </form>
-                    @else
-                        <div class = "alert alert-danger">
+                        @else
+                        <div class="alert alert-danger">
                             <h5> You are not eligible to review this product </h5>
                             <p>
-                                For the trustworthiness of the review, Only customers who purchased 
-                                the product can write a review about the product
+                                Only verified purchasers can submit reviews to ensure authenticity. Thank you for understanding
                             </p>
-                            <a href="{{ url('/'}}" class="btn btn-primary"></a>
+                            <a href="{{ route('productdetail', ['id' => $product->id]) }}" class="btn btn-primary mt-5">Go to Product page</a>
+
                         </div>
 
-                    @endif
+                        @endif
 
-
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <section>
 
 
 
-    @include('layouts/footer')
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </body>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
 
 </html>
