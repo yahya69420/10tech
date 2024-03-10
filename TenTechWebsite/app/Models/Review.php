529ab98b4806c1aa2772476e218038800c791149
Rahul Vasantlal <rahulvasantlal@gmail.com>
2024-03-10 14:54:36 +0000

    created function user , rating and product in rating model to be used by ReviewController

--- TenTechWebsite/app/Models/Review.php
+++ TenTechWebsite/app/Models/Review.php
@@ -9,9 +9,25 @@ class Review extends Model
 {
     use HasFactory;
     protected $table = 'reviews';
-    protected $fillable = [
+    protected $fillable = [ // Defines which model attributes can be mass-assigned safely.
         'user_id',
         'prod_id',
         'user_review'
     ];
+    public function user() 
+    {
+        return $this->belongsTo(User::class); // Indicates a "belongs to" relationship with the User model.
+    }
+
+    public function rating() 
+    {
+        return $this->belongsTo(Rating::class, 'user_id', 'user_id');
+       
+    }
+
+    public function product() 
+    {
+        return $this->belongsTo(Product::class,'prod_id','id');
+    }
+    
 }
