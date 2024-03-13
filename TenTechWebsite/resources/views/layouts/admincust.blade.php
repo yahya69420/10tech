f00a71e5bd994f084a2287f3ca128b1a94d4220a
Muhammad Rashid <220070603@aston.ac.uk>
2024-03-13 09:30:39 +0000

    some cust messages changes

--- TenTechWebsite/resources/views/layouts/admincust.blade.php
+++ TenTechWebsite/resources/views/layouts/admincust.blade.php
@@ -227,17 +227,17 @@
   <h2>Customer Messages</h2>
   <ul class="responsive-table">
     <li class="table-header">
-      <div class="col col-1">User Id</div>
+      <div class="col col-1">Msg Id</div>
       <div class="col col-2">Name</div>
       <div class="col col-3">Message</div>
-      <div class="col col-4">Last Updated</div> 
+      <div class="col col-4">Date Submitted</div> 
     </li>
     @foreach($datamess as $item)
     <li class="table-row">
       <div class="col col-1" data-label="UserId">{{ $item->id}}</div>
       <div class="col col-2" data-label="Name">{{ $item -> name}}</div>
       <div class="col col-3" data-label="Subject">{{ $item->subject }}</div>
-      <div class="col col-4" data-label="Date Created">{{ $item->updated_at }}</div>
+      <div class="col col-4" data-label="Date Created">{{ $item->created_at }}</div>
     </div>
     </li>
     @endforeach
