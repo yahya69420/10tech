3d12efd548788fdaf8c8f7ba0a413ece9f4b1874
Ali <123903017+AQ-GB@users.noreply.github.com>
2024-03-16 21:30:45 +0000

    updated checkout page with existing payment integration

--- TenTechWebsite/resources/views/checkout.blade.php
+++ TenTechWebsite/resources/views/checkout.blade.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Checkout</title>
   <link rel="stylesheet" href="{{ asset('/css/checkout.css') }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css" integrity="sha512-8RxmFOVaKQe/xtg6lbscU9DU0IRhURWEuiI0tXevv+lXbAHfkpamD4VKFQRto9WgfOJDwOZ74c/s9Yesv3VvIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.6.0/cleave.min.js"></script>
+  <script type="module" src="{{ asset('cardFormatterCheckoutPage.js') }}"></script>
   <style>
 
   </style>
@@ -29,14 +32,24 @@
           document.getElementById('sameadr').addEventListener('change', function() {
             if (this.checked) {
               document.getElementById('bi').style.display = 'none';
+              document.getElementById('sameAddressInfoShown').style.display = 'block';
             } else {
               document.getElementById('bi').style.display = 'block';
+              document.getElementById('sameAddressInfoShown').style.display = 'none';
             }
           });
         </script>
         @endif
         <div class="row">
           <div class="col-50">
+            <div class="addressInfoAlreadySame" id="sameAddressInfoShown" style="display: none;">
+              <h1>Address Information</h1>
+              <p>Address Line 1: {{ $userAddress->address_line_1 }}</p>
+              <p>Address Line 2: {{ $userAddress->address_line_2 }}</p>
+              <p>City: {{ $userAddress->city }}</p>
+              <p>Post Code: {{ $userAddress->post_code }}</p>
+              <p>Country: {{ $userAddress->country }}</p>
+            </div>
             <div class="billing-info" id="bi" style="display: block;">
               <h3>Billing Address</h3>
               <label for="fname"><i class="fa fa-user"></i> Full Name</label>
@@ -75,13 +88,68 @@
               // if the checkbox will be checked, just hide the shipping address fields
               document.getElementById('samepay').addEventListener('change', function() {
                 if (this.checked) {
-                  document.getElementById('pi').style.display = 'none';
+                  document.getElementById('pi').style.display = 'block';
+                  // document.getElementById('samePaymentInfoShown').style.display = 'block';
+                  document.getElementById('cname').value = '{{ $userPayments->card_holder_name }}';
+                  document.getElementById('ccnum').value = '{{ $userPayments->card_number }}';
+                  document.getElementById('expmonth').value = '{{ $userPayments->expiry_date }}';
+                  document.getElementById('cvv').value = '{{ $userPayments->cvv }}';
+                  document.getElementById('card_type_icon').className = 'card-icon fab fa-cc-' + '{{ $userPayments->card_type }}';
+                  document.getElementById('cname').readOnly = true;
+                  document.getElementById('ccnum').readOnly = true;
+                  document.getElementById('expmonth').readOnly = true;
+                  document.getElementById('cvv').readOnly = true;
                 } else {
                   document.getElementById('pi').style.display = 'block';
+                  document.getElementById('samePaymentInfoShown').style.display = 'none';
+                  document.getElementById('cname').value = '';
+                  document.getElementById('ccnum').value = '';
+                  document.getElementById('expmonth').value = '';
+                  document.getElementById('cvv').value = '';
+                  document.getElementById('card_type_icon').className = 'card-icon';
+                  document.getElementById('cname').readOnly = false;
+                  document.getElementById('ccnum').readOnly = false;
+                  document.getElementById('expmonth').readOnly = false;
+                  document.getElementById('cvv').readOnly = false;
+
                 }
               });
             </script>
             @endif
+            <div class="paymentInfoAlreadySame" id="samePaymentInfoShown" style="display: none;">
+              <h1>Payment Information</h1>
+              <p>Card Holder Name: {{ $userPayments->card_holder_name }}</p>
+              <p>Card Holder Number: XXXX XXXX XXXX {{ substr($userPayments->card_number, -4) }}</p>
+              <p>Expiry Date: {{ $userPayments->expiry_date }}</p>
+              @php
+              $cartType = $userPayments->card_type;
+              @endphp
+              @if ($userPayments->card_type == 'visa')
+              <span class="card_logo fab fa-cc-visa" style="font-size: 35px; color: <?php echo $userPayments->color; ?>"></span>
+              @php
+              $cartType = $userPayments->card_type;
+              @endphp
+              @elseif ($userPayments->card_type == 'mastercard')
+              <span class="card_logo fab fa-cc-mastercard" style="font-size: 35px; color: <?php echo $userPayments->color; ?>"></span>
+              @php
+              $cartType = $userPayments->card_type;
+              @endphp
+              @elseif ($userPayments->card_type == 'amex')
+              <span class="card_logo fab fa-cc-amex" style="font-size: 35px; color: <?php echo $userPayments->color; ?>"></span>
+              @php
+              $cartType = $userPayments->card_type;
+              @endphp
+              @elseif ($userPayments->card_type == 'discover')
+              <span class="card_logo fab fa-cc-discover" style="font-size: 35px; color: <?php echo $userPayments->color; ?>"></span>
+              @php
+              $cartType = $userPayments->card_type;
+              @endphp
+              @elseif ($userPayments->card_type == 'dinersclub')
+              <span class="card_logo fab fa-cc-diners-club" style="font-size: 35px; color: <?php echo $userPayments->color; ?>"></span>
+              @elseif ($userPayments->card_type == 'jcb')
+              <span class="card_logo fab fa-cc-jcb" style="font-size: 35px; color: <?php echo $userPayments->color; ?>"></span>
+              @endif
+            </div>
             <div class="payment-info" id="pi" style="display: block;">
 
               <h3>Payment</h3>
@@ -89,27 +157,42 @@
               <label for="cname">Name on Card</label>
               <input type="text" id="cname" name="cardname" placeholder="">
               <label for="ccnum">Credit card number</label>
-              <input type="text" id="ccnum" name="cardnumber" placeholder="">
-              <label for="expmonth">Exp Date</label>
-              <input type="text" id="expmonth" name="expmonth" placeholder="MM/YY">
-              <div class="row">
-                <div class="col-50">
-                  <label for="cvv">CVV</label>
-                  <input type="text" id="cvv" name="cvv" placeholder="">
+              <!-- <input type="text" id="ccnum" name="cardnumber" placeholder=""> -->
+              <div class="input-group">
+                <span class="input-group-prepend">
+                  <i id="card_type_icon" class="card-icon"></i>
+                </span>
+                <style>
+                  .card-icon {
+                    font-size: 2rem;
+                    margin-right: 5px;
+                  }
+                </style>
+                <input type="text" id="ccnum" name="cardnumber" placeholder="1234 1234 1234 1234" required>
+                <input id="cardType" name="cardType" value="" hidden readonly required>
+                <input id="cardColour" name="cardColour" value="" hidden readonly required>
+                <label for="expmonth">Exp Date</label>
+                <!-- <input type="text" id="expmonth" name="expmonth" placeholder="MM/YY"> -->
+                <input type="text" id="expmonth" placeholder="06/26" maxlength="5" name="expmonth" required>
+                <div class="row">
+                  <div class="col-50">
+                    <label for="cvv">CVV</label>
+                    <!-- <input type="text" id="cvv" name="cvv" placeholder=""> -->
+                    <input type="text" id="cvv" placeholder="626" maxlength="3" name="cvv">
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-        </div>
-        @foreach ($cartItems as $cartItem)
-        <input type="text" name="user_id" value="{{ auth()->user()->id }}" hidden>
-        <input type="text" name="product_id" value="{{ $cartItem->product_id }}" hidden>
-        <input type="text" name="quantity" value="{{ $cartItem->quantity }}" hidden>
-        <input type="text" name="total" value="{{ $cartItem->total }}" hidden>
-        @endforeach
-        <input type="submit" value="Continue to checkout" class="btn">
-        <!-- <a href="complete"> <input type="submit" value="Continue to checkout" class="btn"></a> -->
+          </div>
+          @foreach ($cartItems as $cartItem)
+          <input type="text" name="user_id" value="{{ auth()->user()->id }}" hidden>
+          <input type="text" name="product_id" value="{{ $cartItem->product_id }}" hidden>
+          <input type="text" name="quantity" value="{{ $cartItem->quantity }}" hidden>
+          <input type="text" name="total" value="{{ $cartItem->total }}" hidden>
+          @endforeach
+          <input type="submit" value="Continue to checkout" class="btn">
+          <!-- <a href="complete"> <input type="submit" value="Continue to checkout" class="btn"></a> -->
       </form>
     </div>
   </div>
