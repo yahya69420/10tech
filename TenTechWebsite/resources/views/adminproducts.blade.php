de1ebea8508662d0bcc3cac23e74a6358c7cb42e
Syed Shah <syedshahyahya@gmail.com>
2024-03-17 16:55:42 +0000

    added modal for adding products, edit and remove buttons

--- TenTechWebsite/resources/views/adminproducts.blade.php
+++ TenTechWebsite/resources/views/adminproducts.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
@@ -11,6 +12,10 @@
 PRODUCTS TABLE 
 </h1>
 <hr>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Add Product
+</button>
 <table class="table">
   <thead>
     <tr>
@@ -18,6 +23,7 @@ PRODUCTS TABLE
       <th scope="col">Name</th>
       <th scope="col">Price</th>
       <th scope="col">Stock</th>
+      <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
@@ -27,9 +33,33 @@ PRODUCTS TABLE
       <td>{{ $product->name }}</td>
       <td>{{ $product->price }}</td>
       <td>{{ $product->stock }}</td>
+      <td>
+        <button class="btn btn-secondary">Edit</button>
+        <button class="btn btn-danger">Remove</button>
+      </td>
     </tr>
     @endforeach
   </tbody>
 </table>
+<!-- add product modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Product</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Add</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
