89bf83e7eb1b39580dcfc0663f95bec7595b54d7
Syed Shah <syedshahyahya@gmail.com>
2024-03-17 17:27:26 +0000

    added modal, added dropdown menu

--- TenTechWebsite/resources/views/adminproducts.blade.php
+++ TenTechWebsite/resources/views/adminproducts.blade.php
@@ -1,65 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">  
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-<h1>
-PRODUCTS TABLE 
-</h1>
-<hr>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  Add Product
-</button>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Name</th>
-      <th scope="col">Price</th>
-      <th scope="col">Stock</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($products as $product)
-    <tr>
-      <th scope="row">{{ $product->id }}</th>
-      <td>{{ $product->name }}</td>
-      <td>{{ $product->price }}</td>
-      <td>{{ $product->stock }}</td>
-      <td>
-        <button class="btn btn-secondary">Edit</button>
-        <button class="btn btn-danger">Remove</button>
-      </td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-<!-- add product modal -->
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Product</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Add</button>
-      </div>
+<body>
+    <h1>
+        PRODUCTS TABLE
+    </h1>
+    <hr>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Add Product
+    </button>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+            <tr>
+                <th scope="row">{{ $product->id }}</th>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->stock }}</td>
+                <td>
+                    <button class="btn btn-secondary">Edit</button>
+                    <button class="btn btn-danger">Remove</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <!-- add product modal -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Product</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+
+                        <div class="mb-3">
+                            <label for="productName" class="form-label">Product Name:</label>
+                            <input type="text" class="form-control" id="productName" placeholder="Enter product name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="productPrice" class="form-label">Product Price:</label>
+                            <input type="number" class="form-control" id="productPrice" placeholder="Enter product price">
+                        </div>
+                        <div class="mb-3">
+                            <label for="productStock" class="form-label">Product Stock:</label>
+                            <input type="number" class="form-control" id="productStock" placeholder="Enter product stock">
+                        </div>
+                        
+                        <div class="dropdown">
+                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown button
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                @foreach($categories as $category)
+                                <li><a class="dropdown-item" href="#">{{$category->name}}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="addProduct()">Add</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </body>
+
 </html>
