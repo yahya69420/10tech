aab674d45d787d2c4103a55a48054fc46ba88e18
Ali <123903017+AQ-GB@users.noreply.github.com>
2024-03-22 11:24:21 +0000

    changed the laptop image attribute

--- TenTechWebsite/database/seeders/ProductSeeder.php
+++ TenTechWebsite/database/seeders/ProductSeeder.php
@@ -46,7 +46,7 @@ class ProductSeeder extends Seeder
         $tabletCategory = Category::create([
             'name' => 'Tablet',
             // 'image' => 'https://images.unsplash.com/photo-1561154464-82e9adf32764?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            'image' => 'tablet5.png',
+            'image' => 'Tablet5.png',
             'price' => '99.99',
 
         ]);
