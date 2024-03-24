6cfffdc31bc8c4092f57b0935225831ecdce771f
Rahul Vasantlal <rahulvasantlal@gmail.com>
2024-03-24 15:04:03 +0000

    created unit test for wishlist controller

--- TenTechWebsite/tests/Feature/WishlistTest.php
+++ TenTechWebsite/tests/Feature/WishlistTest.php
@@ -5,15 +5,17 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Product;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
 class WishlistTest extends TestCase
 {
-    // Need wishlists seeded data for this to work 
-    public function tst_index_function(): void
+
+    public function test_does_wishlist_render_when_empty(): void
     {
         //This simulates a user logging
-        $this->post('/login', [
-            'email' => 'test@test.com',
-            'password' => '1',
+        $this->post('/register', [
+            'email' => 'WishlistRenderWhenEmpty@test.com',
+            'password' => 'TestTest1',
         ]);
         //This simulates a user navigating to the wishlist page after logging in
         $response = $this->get('wishlist');
@@ -24,23 +26,28 @@ class WishlistTest extends TestCase
         // This ensures that the user sees the correct webpage layout and content
         $response->assertViewIs('Wishlist.wishlist');
 
-        // // Assert that the 'wishlist' variable is passed to the view.
-        // This checks that the necessary data (user's wishlist items) is being sent to the view for display.
-        $response->assertViewHas('wishlist');
+        // Asserts that the message 'Your wishlist is empty.' is present in the response
+        $response->assertSee('Your wishlist is empty.');
 
-        // checks if the wishlist variable is not empty 
-        $response->assertViewHas('wishlist', function ($wishlist) {
-            return !$wishlist->isEmpty();
-        });
-
+        // Asserts that the response contains a specific HTML element with the class 'alert-info'
+        $response->assertSee('<div class="alert alert-info"', false);
+        
+        
+        // delete the user as its not linger needed
+        $response = $this->post('/delete-account');
+        // the session for the success message
+        $response->assertSessionHas('success', 'Account deleted successfully');
+        // clear the session data
+        session()->forget('success');
+        // did we get redirected?
+        $response->assertStatus(302);
     }
-
+    
     public function test_add_item_to_wishlist() {
-        
-        
-        $this->post('/login', [
-            'email' => 'test@test.com',
-            'password' => '1',
+        // Register a new user
+        $this->post('/register', [
+            'email' => 'AddItemToWishlist@test.com',
+            'password' => 'TestTest1',
         ]);
 
         // Fetch the product with ID 1 from the database to use in the test.
@@ -48,24 +55,41 @@ class WishlistTest extends TestCase
         $product = Product::findOrFail(1);
         $product_id = $product->id;
 
-
+        // Add the product to the wishlist
         $response = $this->post(route('add-to-wishlist'), [
             'product_id' => $product_id,
-            'user_id' => "1",
+            'user_id' => "5",
         ]);
 
-        $response->assertRedirect()->assertSessionHas('success_wishlist', 'Product added to wishlist!');
+        // Assert that the success message is present in the session
+        $response->assertSessionHas('success_wishlist', 'Product added to wishlist!');
+        
+        // Visit the wishlist page
+        $response = $this->get('wishlist');
+        // Get the wishlist items for the user
+        $wishlist = Wishlist::where('user_id',Auth::id())->get();
 
-        ;
+        // Assert that each item in the wishlist is displayed on the wishlist page
+        foreach ($wishlist as $item) {
+            $response->assertSee( $item->products->name );
+        }
+    
+        // delete the user as its not linger needed
+        $response = $this->post('/delete-account');
+        // the session for the success message
+        $response->assertSessionHas('success', 'Account deleted successfully');
+        // clear the session data
+        session()->forget('success');
+        // did we get redirected?
+        $response->assertStatus(302);
     }
 
-    
     public function test_delete_item_from_wishlist(){
 
         // Log in as a user
-        $this->post('/login', [
-            'email' => 'test@test.com',
-            'password' => '1',
+        $this->post('/register', [
+            'email' => 'DeleteItemFromWishList@test.com',
+            'password' => 'TestTest1',
         ]);
         
         // Get a product to add to the wishlist
@@ -91,6 +115,14 @@ class WishlistTest extends TestCase
             'product_id' =>$product_id,
         ]);
 
+        // delete the user as its not linger needed
+        $response = $this->post('/delete-account');
+        // the session for the success message
+        $response->assertSessionHas('success', 'Account deleted successfully');
+        // clear the session data
+        session()->forget('success');
+        // did we get redirected?
+        $response->assertStatus(302);
     }
 
 }
